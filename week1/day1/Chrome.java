7fbc0ef00063dd8783bccb05edfd6a5d670f913d
Your Name <you@example.com>
2025-06-22 00:27:09 +0530

    intial changes

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2000;
+		char shortcutKey = 'C';
+		
+		
+		System.out.println("Version: "+version +"\n"+"Developer: "+developer+"\n"+"IsBeatvesrion: "+isBeta+"\n"+"ReleaseYear: "+releaseYear
+				+"\n"+"ShortcutKey: "+shortcutKey);
+
+	}
+
+}
---
+++ week1/day1/Datatype.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int releaseYear = 1998;
+		String browserName = "firefox";
+		float browserversion = 100.2f;
+		boolean isVisble = false;
+		char browserlogo = 'f';
+		
+		System.out.println("ReleaseYear: "+releaseYear+"\n"+"Browsername: "+browserName+"\n"+"BrowserVersion: "+browserversion
+				+"\n"+"Visiblity: "+isVisble+"\n"+"Logo: "+browserlogo);
+		
+
+	}
+
+}
